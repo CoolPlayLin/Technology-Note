384d17964d9ade09c7d0d2aabedc7b461a544e4c
CoolPlayLin <CoolPlayLin@qq.com>
2023-01-04 18:08:50 +0800

    Update: Update the module

--- Technology/password.py
+++ Technology/password.py
@@ -1,10 +1,32 @@
+__all__ = ["Dictionary", "Encrypt"]
+
 class Encrypt:
+    def __init__(self) -> None:
+        self.Mode = ["Strict", "Ignore"]
+ 
+    def Encrypt(self, Text:str, Dictionary:dict, Mode="Strict", Add_Spaces=True) -> str:
+        if bool(Add_Spaces):
+            Dictionary[" "] = " "
+        __Undefined = [Undefined for Undefined in Text if Undefined not in Dictionary]
+        _ErrorMessage = "The plaintext entered is empty" if not bool(Text) else "The current pattern does not exist" if Mode not in self.Mode else "The value of the dictionary must be of type dict" if type(Dictionary) is not dict else "Contains characters that are not defined in the dictionary {}{}".format("\n", str( __Undefined)) if bool(__Undefined) and self.Mode.index(Mode) == 0 else None
+        if bool(_ErrorMessage):
+            raise Exception(_ErrorMessage)
+        _ModeType = self.Mode.index(Mode)
+        _Password = ""
+        _PasswordList = [Dictionary[each] for each in Text] if _ModeType == 0 else [Dictionary[each] for each in Text if each in Dictionary] if _ModeType == 1 else None
+        for each in _PasswordList:
+            _Password += each
+
+        return _Password
+
+class Dictionary:
     def __init__(self) -> None:
         self.Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         self.Lowercase = "abcdefghijklmnopqrstuvwxyz"
-        self.Numbers = "123456789"
+        self.Numbers = "1234567890"
 
-    def Dictionary(self, Offset:int, Uppercase=True, Lowercase=False, Number=False, Self_String=None) -> dict:
+
+    def Caesar(self, Offset:int, Uppercase=True, Lowercase=False, Number=False, Self_String=None) -> dict:
         """
         Offset
             Sets the number of character offset bits
@@ -22,23 +44,22 @@ class Encrypt:
             2. The offset parameter cannot exceed the number of strings to be generated as a dictionary, otherwise it will self-explode
             3. You cannot set Uppercase, Lowercase and Number at the same time without setting Self_String, that is, there is no string to generate, otherwise it will explode.
         """
-        Upper = self.Uppercase
-        Lower = self.Lowercase
-        Numbers = self.Numbers
-        Offset = int(Offset)
-        StringList = []
-        Dictionary = {}
-        Source = Self_String if bool(Self_String) else (Upper if bool(Uppercase) else "") + (Lower if bool(Lowercase) else "") + (Numbers if bool(Number) else "")
-        if Source is None:
-            raise Exception("No source data is set")
-        if Offset > len(Source):
-            raise Exception("The offset data exceeds the limit")
-        for each in Source:
-            StringList.append(each)
-        for each in StringList:
-            Dictionary[each] = StringList[StringList.index(each) + Offset] if StringList.index(each) + Offset < len(StringList) else StringList[StringList.index(each) - len(StringList) + Offset]
+        _Upper = self.Uppercase
+        _Lower = self.Lowercase
+        _Numbers = self.Numbers
+        _Offset = int(Offset)
+        _StringList = []
+        _Dictionary = {}
+        _Source = Self_String if bool(Self_String) else (_Upper if bool(Uppercase) else "") + (_Lower if bool(Lowercase) else "") + (_Numbers if bool(Number) else "")
+        _ErrorMessage = "The offset data exceeds the limit" if _Offset > len(_Source) else "The offset data exceeds the limit" if _Source is None else False
+        if bool(_ErrorMessage):
+            raise Exception(_ErrorMessage)
+        for each in _Source:
+            _StringList.append(each)
+        for each in _StringList:
+            _Dictionary[each] = _StringList[_StringList.index(each) + _Offset] if _StringList.index(each) + _Offset < len(_StringList) else _StringList[_StringList.index(each) - len(_StringList) + _Offset]
         
-        return Dictionary
+        return _Dictionary
 
 # 调试
 if __name__ == "__main__":
